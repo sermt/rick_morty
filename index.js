e2868d7378d65099004aab29da3b192bcf79cd83
Miguel Martinez <sermi.cima@gmail.com>
2021-10-20 18:15:37 -0500

    2do commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const axios= require('axios');
-const main =async()=>{
-let response=  await axios.get(' https://rickandmortyapi.com/api/character');
-let {data:{results}} = response;
-let personajes= results.map((item)=> { return {id: item.id, name: item.name}});
-console.log(personajes);
+const axios = require('axios');
+const fs=require('fs').promises;
+const path=require('path');
+const main = async () => {
+    let response = await axios.get(' https://rickandmortyapi.com/api/character');
+    let { data: { results } } = response;
+    let characters = results.map((item) => { return { id: item.id, name: item.name, status: item.status, species: item.species } }).map((extra) =>
+        Object.values(extra).join(','))
+        .join('\n');
+    await fs.writeFile(path.join(__dirname,'data.csv'), characters);
 }
 main();
